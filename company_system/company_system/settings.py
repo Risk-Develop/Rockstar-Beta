f5283e437fb86a78919cefa63d5076d87e25d1b6
Risk-Develop <riskmanagementofficerrwds2023@gmail.com>
2025-11-05 21:28:00 +0800

    modified:   company_system/company_system/settings.py 	modified:   company_system/requirements.txt

--- company_system/company_system/settings.py
+++ company_system/company_system/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '-ules)^-!u@v2&rh5a*u8+mxput4xji)@2x)mb$z=+&i@=w898')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'Rockstar-Beta.onrender.com').split(',')
 
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'company_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # Password validation
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
